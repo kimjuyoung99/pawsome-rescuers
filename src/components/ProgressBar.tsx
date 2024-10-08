41c8e95778aa19154191abcd90861f84d54d0886
kimjuyoung99 <jjassb404@naver.com>
2024-10-08 22:21:55 +0900

    fix : React Query 버전 업, 수정

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,6 +1,7 @@
 // ProgressBar.tsx
 import React from "react";
 import styled from "styled-components";
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 interface ProgressBarProps {
     currentStep: number;
@@ -8,22 +9,42 @@ interface ProgressBarProps {
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ currentStep, totalSteps }) => {
-    const progress = (currentStep / totalSteps) * 100;
+  const progress = (currentStep / totalSteps) * 100;
 
-    return (
-        <StyledElementProgressBar>
-            <ElementBarActive style={{ width: `${progress}%` }} />
-            {Array.from({ length: totalSteps - 1 }, (_, index) => (
-            <ElementBar 
-                key={index} 
-                style={{ 
-                backgroundColor: index < currentStep - 1 ? '#47b1ff' : '#e5e5e5',
-                borderRadius: index === totalSteps - 2 ? '8px 50px 50px 8px' : '8px'
-                }} 
-            />
-            ))}
-        </StyledElementProgressBar>
-    );
+  return (
+      <StyledElementProgressBar>
+          <ElementBarActive style={{ width: `${progress}%` }} />
+          {Array.from({ length: totalSteps - 1 }, (_, index) => (
+          <ElementBar 
+              key={index} 
+              style={{ 
+              backgroundColor: index < currentStep - 1 ? '#47b1ff' : '#e5e5e5',
+              borderRadius: index === totalSteps - 2 ? '8px 50px 50px 8px' : '8px'
+              }} 
+          />
+          ))}
+      </StyledElementProgressBar>
+  );
+};
+
+
+// useProgress 훅 정의
+const PROGRESS_KEY = ['matchingProgress'];
+
+export const useProgress = () => {
+  const queryClient = useQueryClient();
+
+  const { data: currentStep = 1 } = useQuery({
+    queryKey: PROGRESS_KEY,
+    queryFn: () => 1,
+    staleTime: Infinity,
+  });
+
+  const setCurrentStep = (step: number) => {
+    queryClient.setQueryData(PROGRESS_KEY, step);
+  };
+
+  return { currentStep, setCurrentStep };
 };
 
 export default ProgressBar;
